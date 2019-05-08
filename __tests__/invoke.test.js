defcead74ca4d7d79f1e23f557b90bbc0baf4d66
louiswu <louiswu@tencent.com>
2019-05-08 20:02:18 +0800

    ci(ci): set test timeout
    set test timeout

--- __tests__/invoke.test.js
+++ __tests__/invoke.test.js
@@ -7,23 +7,31 @@ sdk.init({
 })
 
 describe('Test Api', () => {
-  test('Test Invoke Correct', async () => {
-    const res = await sdk.invoke({
-      functionName: 'test',
-      data: JSON.stringify({
-        retMsg: 'test'
+  test(
+    'Test Invoke Correct',
+    async () => {
+      const res = await sdk.invoke({
+        functionName: 'test',
+        data: JSON.stringify({
+          retMsg: 'test'
+        })
       })
-    })
-    expect(res).toBe(JSON.stringify('test'))
-  })
+      expect(res).toBe(JSON.stringify('test'))
+    },
+    10 * 1000
+  )
 
-  test('Test Invoke Error', async () => {
-    const res = await sdk.invoke({
-      functionName: 'fake_function',
-      data: JSON.stringify({
-        retMsg: 'test'
+  test(
+    'Test Invoke Error',
+    async () => {
+      const res = await sdk.invoke({
+        functionName: 'fake_function',
+        data: JSON.stringify({
+          retMsg: 'test'
+        })
       })
-    })
-    expect(res.error.code).toBe(LabelResourceNotFound)
-  })
+      expect(res.error.code).toBe(LabelResourceNotFound)
+    },
+    10 * 1000
+  )
 })
